5cc9bb86f160bd6ff993afa21b643c4d45b42d8c
YUZBAS1EA <145233685+yuzbas1ea@users.noreply.github.com>
2025-01-22 01:46:42 +0300

    Update CariTakipWeb.py

--- CariTakip/CariTakipWeb.py
+++ CariTakip/CariTakipWeb.py
@@ -362,4 +362,4 @@ if __name__ == '__main__':
 
         db.session.commit()
     
-    app.run(debug=True, port=5001, host='0.0.0.0')  # Parantezlerin doğru kapatıldığından emin olun
+    app.run(debug=True, port=5001, host='0.0.0.0')
